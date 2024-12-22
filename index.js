2768a63f7c5a6549ae98520cea6bb5a8a0c7d0c0
raselworshop <raselworshop@gmail.com>
2024-12-23 02:23:50 +0800

    added the method for details, and method for fetching all data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,33 +15,56 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const serviceCollection = client.db('ServiceReview').collection('Services')
+        const serviceCollection = client.db('ServiceReview').collection('Services')
 
-    app.get('/services', async (req, res) => {
-        const cursor = serviceCollection.find().limit(6);
-        const result = await cursor.toArray();
-        res.send(result)
-    })
+        app.post('/user/add/service', async (req, res) => {
+            const serviceData = req.body;
+            console.log(serviceData);
+            const result = await serviceCollection.insertOne(serviceData);
+            res.send(result)
+        })
+        // limited service view 
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find().limit(6);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // all services page 
+        app.get('/all/services', async (req, res) => {
+
+            const result = await serviceCollection.find().toArray();
+            res.send(result)
+        })
+
+        // details page 
+        app.get('/services/details/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: new ObjectId(id) };
+            const result = await serviceCollection.findOne(query);
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -50,6 +73,6 @@ app.get('/', async (req, res) => {
     res.send('Service Review System is running')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`review is waiting :${port}`)
 })
